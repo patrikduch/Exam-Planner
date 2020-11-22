1ee87a078be3ac43ef46a4c2446616a7df54aef8
patrikduch <duchpatrik@icloud.com>
2020-11-22 16:29:23 +0100

    Student view: exam listing

--- webapp/app/infrastructure/repositories/StudentRepository.php
+++ webapp/app/infrastructure/repositories/StudentRepository.php
@@ -28,7 +28,16 @@ class StudentRepository implements  IStudentRepository {
      */
     public function getStudentDetails(int $userId)
     {
-       $resultSet = $this->database->table('Student')->fetch('SELECT * FROM Student WHERE id = ?', $userId);
+       $resultSet = $this->database->query(
+           'SELECT SC.student_code, S.first_name, S.surname, C.title AS course_title,
+           C.credits, C.lecture_hours, C.practice_hours, C.classification, C.annotation
+           FROM StudentCourse SC 
+           JOIN Student S ON SC.student_code = S.student_code
+           JOIN Course C ON SC.course_code = C.course_code
+            WHERE S.user_id = (
+	            SELECT id  FROM User WHERE id = 1);
+	       ', $userId
+       );
 
        return $resultSet;
     }
